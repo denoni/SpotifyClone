545e99b957eeb04296c571767ce3975e84782272
Gabriel <gabrieldenonifake@gmail.com>
2021-08-31 18:53:26 -0300

    Add assets(albumnCovers, icons, etc...)

---
+++ SpotifyCloneProject/SpotifyClone/Extensions/View+RoundedSpecificSide.swift
@@ -0,0 +1,25 @@
+//
+//  View+RoundedSpecificSide.swift
+//  SpotifyClone
+//
+//  Created by Gabriel on 8/31/21.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
