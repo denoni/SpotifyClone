94438a7938e45d6b4e152991966a22922c330208
Gabriel <gabrieldenonifake@gmail.com>
2021-08-31 19:32:18 -0300

    Add horizontal scroll view of recently played songs (https://imgur.com/a/ddttDMZ)

--- SpotifyCloneProject/SpotifyClone/ContentView.swift
+++ SpotifyCloneProject/SpotifyClone/ContentView.swift
@@ -9,9 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
 
-// TODO: Reduce duplicated code
-// TODO: Convert to arrays and render using ForEach
-
   var body: some View {
       ZStack {
         Color.spotifyDarkGray.ignoresSafeArea()
--- SpotifyCloneProject/SpotifyClone/Views/HomeScreen.swift
+++ SpotifyCloneProject/SpotifyClone/Views/HomeScreen.swift
@@ -5,6 +5,9 @@
 //  Created by Gabriel on 8/31/21.
 //
 
+// TODO: Reduce duplicated code
+// TODO: Convert to arrays and render using ForEach
+
 import SwiftUI
 
 struct HomeScreen: View {
@@ -14,6 +17,8 @@ struct HomeScreen: View {
       VStack(alignment: .leading) {
         SmallSongCardsGrid()
           .padding(.horizontal, 25)
+          .padding(.bottom, 50)
+        RecentlyPlayedScrollView()
           .padding(.bottom, 50)
       }.padding(.vertical, 25)
     }
@@ -61,6 +66,39 @@ struct SmallSongCardsGrid: View {
                         title: "Your Mix 1")
           SmallSongCard(image: Image("bohemian-rhapsody-cover"),
                         title: "Bohemian Rhapsody")
+        }
+      }
+    }
+  }
+}
+
+struct RecentlyPlayedScrollView: View {
+  var body: some View {
+    VStack(spacing: 12) {
+      Text("Recently Played").font(.avenir(.heavy, size: 26))
+        .frame(maxWidth: .infinity, alignment: .topLeading)
+        .padding(.leading, 25)
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top,spacing: 20) {
+          Spacer(minLength: 5)
+          SmallSongItem(coverImage: Image("hip-hop-controller-cover"),
+                        title: "Hip Hop Controller")
+          SmallSongItem(coverImage: Image("iu-cover"),
+                        title: "IU",
+                        isArtistProfile: true)
+          SmallSongItem(coverImage: Image("liked-songs-cover"),
+                        title: "Liked Songs")
+          SmallSongItem(coverImage: Image("late-night-lofi-cover"),
+                        title: "Late Night Lofi")
+          SmallSongItem(coverImage: Image("we-love-you-tecca-cover"),
+                        title: "We Love You Tecca")
+          SmallSongItem(coverImage: Image("avicii-cover"),
+                        title: "AVICII",
+                        isArtistProfile: true)
+          SmallSongItem(coverImage: Image("sweetener-cover"),
+                        title: "Sweetener")
+          SmallSongItem(coverImage: Image("viral-hits-cover"),
+                        title: "Viral Hits")
         }
       }
     }
