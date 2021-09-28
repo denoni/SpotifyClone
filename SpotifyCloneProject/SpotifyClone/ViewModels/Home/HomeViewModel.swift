738740dc9feb34382f38eff2dfe9990866923825
Gabriel <gabrieldenonifake@gmail.com>
2021-09-27 21:36:00 -0300

    Add a check for duplicated items received from API. Duplicated items were causing crashes on the app. Also reduced code duplication.

--- SpotifyCloneProject/SpotifyClone/ViewModels/Home/HomeViewModel.swift
+++ SpotifyCloneProject/SpotifyClone/ViewModels/Home/HomeViewModel.swift
@@ -18,23 +18,23 @@ import SwiftUI
 class HomeViewModel: ObservableObject {
   var api = APIFetchingDataHomePage()
   @Published var mainViewModel: MainViewModel
-
+  
   @Published var isLoading = [Section:Bool]()
   @Published var mediaCollection = [Section:[SpotifyModel.MediaItem]]()
   @Published var numberOfLoadedItemsInSection = [Section:Int]()
-
+  
   @Published var mediaDetailViewModel = MediaDetailViewModel()
   @Published var currentSubPage: HomeSubpage = .none
-
+  
   @Published var veryFirstImageInfo = RemoteImageModel(urlString: "")
-
+  
   enum HomeSubpage {
     case none
     case playlistDetail
     case trackDetail
     case stillToBeMade // TODO: Create the rest of the real detail screens
   }
-
+  
   init(mainViewModel: MainViewModel) {
     self.mainViewModel = mainViewModel
     // Populate isLoading and medias with all possible section keys
@@ -43,10 +43,10 @@ class HomeViewModel: ObservableObject {
       mediaCollection[section] = []
       numberOfLoadedItemsInSection[section] = 0
     }
-
+    
     fetchHomeData()
   }
-
+  
   enum Section: String, CaseIterable {
     case smallSongCards = "Small Song Card Items"
     case userFavoriteTracks = "Songs You Love"
@@ -63,13 +63,13 @@ class HomeViewModel: ObservableObject {
     case playlistRewind2000s = "2000s Rewind"
     case playlistRewind2010s = "2010s Rewind"
   }
-
+  
   func fetchHomeData() {
     for dictKey in isLoading.keys { isLoading[dictKey] = true }
-
+    
     if mainViewModel.authKey != nil {
       let accessToken = mainViewModel.authKey!.accessToken
-
+      
       getSmallSongCardItems(accessToken: accessToken)
       getUserFavoriteTracks(accessToken: accessToken)
       getUserRecentlyPlayed(accessToken: accessToken)
@@ -86,145 +86,136 @@ class HomeViewModel: ObservableObject {
       getPlaylistThisIsX(accessToken: accessToken)
     }
   }
-
-
-
+  
+  
+  
   // MARK: - Calls to fetch data
-
+  
   private func getSmallSongCardItems(accessToken: String, loadingMore: Bool = false) {
     fetchDataFor(Section.smallSongCards, with: accessToken)
   }
-
+  
   private func getNewReleases(accessToken: String, loadingMore: Bool = false) {
     fetchDataFor(Section.newReleases, with: accessToken)
   }
-
+  
   private func getTopPodcasts(accessToken: String) {
     fetchDataFor(Section.topPodcasts, with: accessToken)
   }
-
+  
   private func getUserRecentlyPlayed(accessToken: String) {
     fetchDataFor(Section.recentlyPlayed, with: accessToken)
   }
-
+  
   private func getUserFavoriteTracks(accessToken: String) {
     fetchDataFor(Section.userFavoriteTracks, with: accessToken)
   }
-
+  
   private func getUserFavoriteArtists(accessToken: String) {
     fetchDataFor(Section.userFavoriteArtists, with: accessToken)
   }
-
+  
   private func getFeaturedPlaylists(accessToken: String) {
     fetchDataFor(Section.featuredPlaylists, with: accessToken)
   }
-
+  
   private func getPlaylistYearRewinds(accessToken: String, year: Section) {
     fetchDataFor(year, with: accessToken)
   }
-
+  
   private func getPlaylistThisIsX(accessToken: String) {
     fetchDataFor(Section.playlistThisIsX, with: accessToken)
   }
-
+  
   private func getTopTracksFromArtist(accessToken: String) {
     fetchDataFor(Section.artistTopTracks, with: accessToken)
   }
-
-
-
+  
+  
+  
   // MARK: - Fetch Data From API
-
+  
   func fetchDataFor(_ section: Section, with accessToken: String) {
     let numberOfItemsInEachLoad = 10
     let currentNumberOfLoadedItems = getNumberOfLoadedItems(for: section)
     increaseNumberOfLoadedItems(for: section, by: numberOfItemsInEachLoad)
-
+    
     guard numberOfLoadedItemsInSection[section]! <= 50 else {
       return
     }
-
+    
     DispatchQueue.main.async { [unowned self] in
       switch section {
-
+      
       // MARK: - Track Responses
-
+      
       // MARK: Small Song Card Items
       case .smallSongCards:
         api.getTrack(using: .userFavoriteTracks, with: accessToken) { tracks in
-          mediaCollection[section] = tracks
-          isLoading[section] = false
-
+          trimAndCommunicateResult(section: section, medias: tracks)
           setVeryFirstImageInfoBasedOn(tracks[0].imageURL)
         }
-
+        
       // MARK: Recently Played
       case .recentlyPlayed:
         api.getTrack(using: .userRecentlyPlayed, with: accessToken) { tracks in
-          mediaCollection[section] = tracks
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: tracks)
         }
-
+        
       // MARK: User Favorite Tracks
       case .userFavoriteTracks:
         api.getTrack(using: .userFavoriteTracks,
                      with: accessToken,
                      limit: numberOfItemsInEachLoad,
                      offset: currentNumberOfLoadedItems) { tracks in
-          mediaCollection[section]! += tracks
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: tracks, loadMoreEnabled: true)
         }
-
-
+        
+        
       // MARK: - Artist Responses
-
+      
       // MARK: User Favorite Artists
       case .userFavoriteArtists:
         api.getUserFavoriteArtists(accessToken: accessToken) { artists in
-          mediaCollection[section] = artists
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: artists)
         }
-
+        
       // MARK: New Releases
       case .newReleases:
         api.getNewReleases(accessToken: accessToken,
                            limit: numberOfItemsInEachLoad,
-                           offset: currentNumberOfLoadedItems) { album in
-          mediaCollection[section]! += album
-          isLoading[section] = false
-
+                           offset: currentNumberOfLoadedItems) { albums in
+          trimAndCommunicateResult(section: section, medias: albums, loadMoreEnabled: true)
+          
         }
-
+        
       // MARK: Top Podcasts
       case .topPodcasts:
         api.getTopPodcasts(accessToken: accessToken,
                            limit: numberOfItemsInEachLoad,
                            offset: currentNumberOfLoadedItems) { podcasts in
-          mediaCollection[section]! += podcasts
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: podcasts, loadMoreEnabled: true)
         }
-
+        
       // MARK: Featured Playlists
       case .featuredPlaylists:
         api.getFeaturedPlaylists(accessToken: accessToken) { playlists in
-          mediaCollection[section] = playlists
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: playlists)
         }
-
+        
       // MARK: Playlist This is X
       case .playlistThisIsX:
         let keyWord = "this is"
         api.getPlaylistsWith(keyWord: keyWord, accessToken: accessToken,
                              limit: numberOfItemsInEachLoad,
                              offset: currentNumberOfLoadedItems) { playlists in
-          mediaCollection[section]! += playlists
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: playlists, loadMoreEnabled: true)
         }
-
+        
       // MARK: Playlist Year Rewinds
       case .playlistRewind2010s, .playlistRewind2000s, .playlistRewind90s,
            .playlistRewind80s, .playlistRewind70s :
-
+        
         var keyWord = "top hits of "
         switch section {
         case .playlistRewind2010s:
@@ -240,12 +231,11 @@ class HomeViewModel: ObservableObject {
         default:
           fatalError("Year not defined or the section is not a year.")
         }
-
+        
         api.getPlaylistsWith(keyWord: keyWord, accessToken: accessToken) { playlists in
-          mediaCollection[section]! = playlists
-          isLoading[section] = false
+          trimAndCommunicateResult(section: section, medias: playlists)
         }
-
+        
       // MARK: Artist's Top Tracks
       case .artistTopTracks:
         var artistID = ""
@@ -254,54 +244,71 @@ class HomeViewModel: ObservableObject {
           let userMostFavoriteArtist = artists[0]
           artistID = userMostFavoriteArtist.id
           mediaCollection[section]!.insert(artists[0], at: 0)
-
+          
           // Add the artist's top songs
           api.getTrack(using: .topTracksFromArtist,
                        with: accessToken,
-                       ifArtistsUseId: artistID) { trackItems in
-            mediaCollection[section]!.append(contentsOf: trackItems)
-            isLoading[section] = false
+                       ifArtistsUseId: artistID) { tracks in
+            trimAndCommunicateResult(section: section, medias: tracks, loadMoreEnabled: true)
           }
         }
       default:
         fatalError("Tried to fetch data for a type not specified in the function declaration(fetchDataFor).")
-
+        
       }
     }
   }
-
-
-
+  
+  
+  
   // MARK: - Auxiliary Functions
-
+  
   func getNumberOfLoadedItems(for section: Section) -> Int {
     return numberOfLoadedItemsInSection[section]!
   }
-
+  
   func increaseNumberOfLoadedItems(for section: Section, by amount: Int) {
     if numberOfLoadedItemsInSection[section]! <= 50 {
       numberOfLoadedItemsInSection[section]! += amount
     }
   }
-
-
-
+  
+  func trimAndCommunicateResult(section: Section, medias: [SpotifyModel.MediaItem], loadMoreEnabled: Bool = false) {
+    var noDuplicateMedias = [SpotifyModel.MediaItem]()
+    var mediaIDs = [String]()
+    
+    for media in medias {
+      if !mediaIDs.contains(media.id) {
+        mediaIDs.append(media.id)
+        noDuplicateMedias.append(media)
+      }
+    }
+    
+    if loadMoreEnabled {
+      mediaCollection[section]! += noDuplicateMedias
+    } else {
+      mediaCollection[section] = noDuplicateMedias
+    }
+    
+    isLoading[section] = false
+  }
+  
+  
   // MARK: - Non-api Related Functions
-
+  
   func goToNoneSubpage() {
     currentSubPage = .none
   }
-
+  
   func changeSubpageTo(_ subPage: HomeSubpage,
                        mediaDetailViewModel: MediaDetailViewModel,
                        withData data: SpotifyModel.MediaItem) {
     currentSubPage = subPage
     mediaDetailViewModel.media = data
   }
-
+  
   func setVeryFirstImageInfoBasedOn(_ firstImageURL: String) {
     veryFirstImageInfo = RemoteImageModel(urlString: firstImageURL)
   }
-
   
 }
