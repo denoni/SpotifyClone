91d46a68a5d96d7e2c30e5a5940983c55939d506
Gabriel <gabrieldenonifake@gmail.com>
2021-10-20 11:16:26 -0300

    Add the items that will be fixed in the MyLibraryItemsScrollView.

--- SpotifyCloneProject/SpotifyClone/Views/MyLibraryScreen/MyLibraryScreen.swift
+++ SpotifyCloneProject/SpotifyClone/Views/MyLibraryScreen/MyLibraryScreen.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct MyLibraryScreen: View {
-  @Environment(\.topSafeAreaSize) var topSafeAreaSize
-
   static let mediaForTest = SpotifyModel.MediaItem(title: "Don't Stop Me Now",
                                                    previewURL: "",
                                                    imageURL: "https://i.scdn.co/image/ab67616d0000b273008b06ec71019afd70153889",
@@ -25,7 +23,25 @@ struct MyLibraryScreen: View {
                                                                                                     id: "abc",
                                                                                                     album: nil)))
 
+
   let mediaTestArray = Array(repeating: mediaForTest, count: 5)
+
+  var body: some View {
+    VStack {
+      ZStack {
+        ScrollView(showsIndicators: false) {
+          MyLibraryItemsScrollView(medias: mediaTestArray)
+        }
+        .padding(.horizontal, Constants.paddingStandard)
+
+        LibraryTopBar()
+      }
+    }
+  }
+}
+
+fileprivate struct LibraryTopBar: View {
+  @Environment(\.topSafeAreaSize) var topSafeAreaSize
 
   var body: some View {
     VStack {
@@ -61,44 +77,44 @@ struct MyLibraryScreen: View {
       .frame(height: 60 + topSafeAreaSize)
       .frame(maxWidth: .infinity)
       .ignoresSafeArea()
-
-
-      ScrollView(showsIndicators: false) {
-        MyLibraryItemsScrollView(medias: mediaTestArray)
-      }
-      .padding(.horizontal, Constants.paddingStandard)
-      .padding(.bottom, Constants.paddingBottomSection)
       Spacer()
     }
+    .frame(maxHeight: .infinity)
   }
 }
 
-struct MyLibraryItemsScrollView: View {
+fileprivate struct MyLibraryItemsScrollView: View {
   @State var medias: [SpotifyModel.MediaItem]
-  var mockData: [(image: Image, title: String, subTitle: String)] = [(Image("liked-songs-cover"), "Liked Songs", "Playlist • 33 songs"),
-                                                                   (Image("your-episodes-cover"), "Your Episodes", "Saved and downloaded episodes")]
+
+  init(medias: [SpotifyModel.MediaItem]) {
+    var mediasWithPinnedItems: [SpotifyModel.MediaItem]
+    mediasWithPinnedItems = [FixedSectionsItems.likedSongs, FixedSectionsItems.yourEpisodes]
+    mediasWithPinnedItems += medias
+    self.medias = mediasWithPinnedItems
+  }
 
   var body: some View {
     VStack {
-      ForEach(mockData, id: \.title) { media in
+      ForEach(medias) { media in
         HStack(spacing: Constants.spacingSmall) {
           Rectangle()
             .foregroundColor(.spotifyMediumGray)
-            .overlay(media.image.resizeToFit())
-//            .overlay(RemoteImage(urlString: media.imageURL))
+            .overlay(RemoteImage(urlString: media.imageURL))
             .frame(width: 80, height: 80)
           VStack(alignment: .leading) {
             Text(media.title)
               .font(.avenir(.heavy, size: Constants.fontSmall))
               .lineLimit(1)
               .padding(.trailing, Constants.paddingLarge)
-            HStack {
-              Image(systemName: "pin.fill")
-                .resizeToFit()
-                .frame(height: 12)
-                .foregroundColor(.spotifyGreen)
-                .padding(.trailing, -3)
-              Text(media.subTitle) // TODO: Show real data
+            HStack(spacing: 0) {
+              if media.id == "liked-songs" || media.id == "your-episodes" {
+                Image(systemName: "pin.fill")
+                  .resizeToFit()
+                  .frame(height: 12)
+                  .foregroundColor(.spotifyGreen)
+                  .padding(.trailing, 3)
+              }
+              Text(media.previewURL) // TODO: Show real data
                 .font(.avenir(.medium, size: Constants.fontXSmall))
                 .opacity(Constants.opacityHigh)
                 .lineLimit(1)
@@ -113,8 +129,21 @@ struct MyLibraryItemsScrollView: View {
         }
       }
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .padding(.bottom, Constants.paddingBottomSection)
+    .padding(.top, 60 + Constants.paddingStandard)
   }
 }
+
+
+
+fileprivate struct FixedSectionsItems {
+  static let likedSongs = SpotifyModel.MediaItem(title: "Liked Songs", previewURL: "Playlist • 33 songs", imageURL: "https://i.ibb.co/w4jgnbs/liked-songs-300.png", authorName: ["Spotify"], author: nil, mediaType: .playlist, id: "liked-songs", details: SpotifyModel.DetailTypes.playlists(playlistDetails:SpotifyModel.PlaylistDetails(description: "Saved and downloaded episodes", playlistTracks: SpotifyModel.PlaylistTracks(numberOfSongs: 0, href: ""), owner: SpotifyModel.MediaOwner(displayName: "", id: "0"), id: "")))
+
+  static let yourEpisodes = SpotifyModel.MediaItem(title: "Your Episodes", previewURL: "Saved & downloaded episodes", imageURL: "https://i.ibb.co/bbZH8pS/Screen-Shot-2021-10-20-at-12-37-06-AM.png", authorName: ["Spotify"], author: nil, mediaType: .playlist, id: "your-episodes", details: SpotifyModel.DetailTypes.playlists(playlistDetails:SpotifyModel.PlaylistDetails(description: "", playlistTracks: SpotifyModel.PlaylistTracks(numberOfSongs: 0, href: ""), owner: SpotifyModel.MediaOwner(displayName: "", id: "1"), id: "")))
+}
+
+
 
 struct MyLibraryScreen_Previews: PreviewProvider {
   static var previews: some View {
