a03f5ab310be573b33e2fbe766993cfb5f231838
Gabriel <gabrieldenonifake@gmail.com>
2021-10-23 01:29:14 -0300

    Add back code that shouldn't have been deleted.

--- SpotifyCloneProject/SpotifyClone/Views/HomeScreen/HomeScreenViews/RecentlyPlayedScrollView.swift
+++ SpotifyCloneProject/SpotifyClone/Views/HomeScreen/HomeScreenViews/RecentlyPlayedScrollView.swift
@@ -24,6 +24,11 @@ struct RecentlyPlayedScrollView: View {
           ForEach(medias) { media in
             SmallSongItem(imageURL: media.imageURL,
                           title: media.title)
+              .onTapGesture {
+                homeVM.changeSubpageTo(.trackDetail,
+                                       mediaDetailVM: mediaDetailVM,
+                                       withData: media)
+              }
               .onAppear { homeVM.homeCachedImageURLs.append(media.imageURL) }
               .onDisappear{
                 if homeVM.homeCachedImageURLs.count > 25 {
